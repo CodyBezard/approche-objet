0b752089045ab7b294867249a85964e7902619f0
Cody Bezard <cody.bezard@gmail.com>
2024-10-04 12:27:11 +0200

    troisieme commit

--- src/main/java/entites/TestPersonne.java
+++ src/main/java/entites/TestPersonne.java
@@ -8,6 +8,7 @@ public class TestPersonne {
         Personne pers1 = new Personne();
         pers1.nom = "Dupont";
         pers1.prenom = "Michel";
+        pers1.adresse = new AdressePostale();
         pers1.adresse.numeroRue = 5;
         pers1.adresse.libelleRue = "Rue de la liberté";
         pers1.adresse.codePostal = 69000;
@@ -17,6 +18,7 @@ public class TestPersonne {
         Personne pers2 = new Personne();
         pers2.nom = "Dupont";
         pers2.prenom = "Michel";
+        pers2.adresse = new AdressePostale();
         pers2.adresse.numeroRue = 8;
         pers2.adresse.libelleRue = "Rue de la democratie";
         pers2.adresse.codePostal = 31000;

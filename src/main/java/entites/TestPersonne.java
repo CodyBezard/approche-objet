38e789f8b16cd8d52df60d79bcba57812871df0a
Cody Bezard <cody.bezard@gmail.com>
2024-10-04 11:57:49 +0200

    deuxième commit

--- src/main/java/entites/TestPersonne.java
+++ src/main/java/entites/TestPersonne.java
@@ -5,26 +5,21 @@ import entites2.Personne;
 public class TestPersonne {
     public static void main(String args[]) {
 
-        AdressePostale adr1 = new AdressePostale();
-        adr1.numeroRue = 5;
-        adr1.libelleRue = "Rue de la liberté";
-        adr1.codePostal = 69000;
-        adr1.ville = "Lyon";
-
         Personne pers1 = new Personne();
         pers1.nom = "Dupont";
         pers1.prenom = "Michel";
-        pers1.adresse = adr1;
+        pers1.adresse.numeroRue = 5;
+        pers1.adresse.libelleRue = "Rue de la liberté";
+        pers1.adresse.codePostal = 69000;
+        pers1.adresse.ville = "Lyon";
 
-        AdressePostale adr2 = new AdressePostale();
-        adr2.numeroRue = 8;
-        adr2.libelleRue = "Rue de la démocratie";
-        adr2.codePostal = 31000;
-        adr2.ville = "Toulouse";
 
         Personne pers2 = new Personne();
         pers2.nom = "Dupont";
         pers2.prenom = "Michel";
-        pers2.adresse = adr2;
+        pers2.adresse.numeroRue = 8;
+        pers2.adresse.libelleRue = "Rue de la democratie";
+        pers2.adresse.codePostal = 31000;
+        pers2.adresse.ville = "Toulouse";
     }
 }
